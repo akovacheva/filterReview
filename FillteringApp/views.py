9a2aed93d3e1081e7a52a1dd717f70df0f3bb2c1
akovacheva <anna_kovacheva@hotmail.com>
2023-03-16 16:35:36 +0100

    Filter by rating done - only the selected rating

--- FillteringApp/views.py
+++ FillteringApp/views.py
@@ -5,14 +5,20 @@ from FillteringApp.models import Review
 
 
 def ReviewFilterView(request):
-    qs = Review.objects.distinct().all()
+    qs = Review.objects.all()
+    rs = Review.objects.values_list('rating').distinct()
+    numbers = [r[0] for r in rs]
+
     rating_query =  request.GET.get("rating")
+    reviewText_query = request.GET.get("reviewText")
 
     if rating_query != '' and rating_query is not None:
         qs = qs.filter(rating=rating_query)
+    if reviewText_query != '' and reviewText_query is not None:
+        qs = qs.filter(reviewText=reviewText_query)
     context = {
-        'queryset': qs
-
+        'queryset': qs,
+        'ratingset': numbers
     }
     return render(request, "review_form.html", context)
 
