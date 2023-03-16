dbc5793365e42b3911a107b8cf8f483b88f889a0
akovacheva <anna_kovacheva@hotmail.com>
2023-03-16 21:48:57 +0100

    final commit

--- FillteringApp/views.py
+++ FillteringApp/views.py
@@ -14,22 +14,39 @@ def ReviewFilterView(request):
     rating_order = request.GET.get("rating_order")
 
     date_order = request.GET.get("date_order")
+    #
+    # if rating_query:
+    #     qs = qs.filter(rating__gte=rating_query)
+    #
+    # if prioritize_text_query == "Yes":
+    #     qs = qs.order_by('-reviewText')
+    #
+    #
+    # if rating_order == "Highest First":
+    #     qs = qs.order_by('-rating')
+    # elif rating_order == "Lowest First":
+    #     qs = qs.order_by('rating')
+    #
+    # if date_order == "Newest First":
+    #     qs = qs.order_by('-reviewCreatedOnDate')
+    # elif rating_order == "Oldest First":
+    #     qs = qs.order_by('reviewCreatedOnDate')
 
-    if rating_query:
-        qs = qs.filter(rating__gte=rating_query)
-
-    if prioritize_text_query == "Yes":
-        qs = qs.order_by('-reviewText')
-
-    if rating_order == "Highest First":
-        qs = qs.order_by('-rating')
+    if rating_query and prioritize_text_query == "Yes":
+        qs = qs.filter(rating__gte=rating_query).order_by('-reviewText', '-rating', '-reviewCreatedOnDate')
+    elif rating_query:
+        qs = qs.filter(rating__gte=rating_query).order_by('-rating', '-reviewCreatedOnDate', '-reviewText')
+    elif prioritize_text_query == "Yes":
+        qs = qs.order_by('-reviewText', '-rating', '-reviewCreatedOnDate')
+    elif rating_order == "Highest First":
+        qs = qs.order_by('-rating', '-reviewCreatedOnDate', '-reviewText')
     elif rating_order == "Lowest First":
-        qs = qs.order_by('rating')
+        qs = qs.order_by('rating', '-reviewCreatedOnDate', '-reviewText')
+    elif date_order == "Newest First":
+        qs = qs.order_by('-reviewCreatedOnDate', '-rating', '-reviewText')
+    elif date_order == "Oldest First":
+        qs = qs.order_by('reviewCreatedOnDate', '-rating', '-reviewText')
 
-    if date_order == "Newest First":
-        qs = qs.order_by('-reviewCreatedOnDate')
-    elif rating_order == "Oldest First":
-        qs = qs.order_by('reviewCreatedOnDate')
 
     context = {
         'queryset': qs,
