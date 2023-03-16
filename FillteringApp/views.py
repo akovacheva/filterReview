3e0343a3b67d793896cccc58612cd69cebef57c8
akovacheva <anna_kovacheva@hotmail.com>
2023-03-16 19:50:01 +0100

    prioritize by text

--- FillteringApp/views.py
+++ FillteringApp/views.py
@@ -1,24 +1,26 @@
+from django.db.models import Q
 from django.shortcuts import render
-
-# Create your views here.
 from FillteringApp.models import Review
-
 
 def ReviewFilterView(request):
     qs = Review.objects.all()
+
     rs = Review.objects.values_list('rating').distinct()
     numbers = [r[0] for r in rs]
 
-    rating_query =  request.GET.get("rating")
-    reviewText_query = request.GET.get("reviewText")
+    rating_query = request.GET.get("rating")
+    prioritize_text_query = request.GET.get("Prioritizebytext")
 
     if rating_query != '' and rating_query is not None:
         qs = qs.filter(rating=rating_query)
-    if reviewText_query != '' and reviewText_query is not None:
-        qs = qs.filter(reviewText=reviewText_query)
+
+    if prioritize_text_query == "Yes":
+        qs = qs.filter(Q(reviewText__isnull=False) & ~Q(reviewText='')).order_by('-reviewText', '-rating')
+
+
     context = {
         'queryset': qs,
-        'ratingset': numbers
+        'ratingset': numbers,
+        'prioritize_text_query': prioritize_text_query
     }
     return render(request, "review_form.html", context)
-
